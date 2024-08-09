7f4765d79f4e4c4ba474f8b3f9799a1cc661c373
sinke237 <enowsinke@gmail.com>
2024-08-09 18:39:08 +0100

    feat(backend): init backend module

---
+++ readwell-backend/src/main/java/com/prominds/readwell_backend/ReadwellBackendApplication.java
@@ -0,0 +1,13 @@
+package com.prominds.readwell_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadwellBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadwellBackendApplication.class, args);
+	}
+
+}
---
+++ readwell-backend/src/test/java/com/prominds/readwell_backend/ReadwellBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prominds.readwell_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadwellBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
